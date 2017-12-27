e441130985438a8bf321772ddfb660091881885e
Geurt Lagemaat <geurtlagemaat@gmail.com>
2017-12-27 21:14:15 +0100

    work in progress

---
+++ common.py
@@ -0,0 +1,249 @@
+"""Shared functions for Fabric scripts"""
+import os
+import zipfile
+import shutil
+import time
+import string
+import random
+from os.path import join
+from posixpath import join as posixjoin
+from contextlib import closing
+
+from fabric.api import run, cd, sudo, env
+from fabric.decorators import task
+
+BLIKNET_BASE_DIR = '/opt/bliknet'
+
+def generate_password(length=8):
+    """Generate a random password with the given length"""
+    chars = string.ascii_letters + string.digits
+    random.seed = (os.urandom(1024))
+    return ''.join(random.choice(chars) for _ in range(length))
+
+
+def collect_requirements(requirements_path, repository_path, target_path):
+    """Copy requirement packages, also creating a list of filenames of packages that should be installed."""
+    required_filenames = get_dependency_filenames(requirements_path, repository_path)
+    os.makedirs(target_path)
+    with open(join(target_path, 'requirement-filenames.txt'), 'w') as f:
+        for fn in required_filenames:
+            shutil.copy(fn, target_path)
+
+            f.write('%s\n' % os.path.basename(fn))
+
+    filter_requirements(requirements_path, os.path.join(target_path, 'filtered-requirements.txt'))
+
+
+def install_virtualenv_contents_setup(venv_path, package_repository_path, package_list_path):
+    """Install or upgrade requirements into an existing virtualenv using setup.py"""
+    # Create temporary working directory
+    work_dir = sudo('mktemp -d')
+
+    # Read contents of file with package names
+    reqs_file_contents = run('cat "{package_list_path}"'.format(**locals()))
+    
+    for fn in reqs_file_contents.split('\n'):
+        fn = fn.strip()
+        if fn == '':
+            continue
+
+        # Unpack the package into the temporary directory
+        with cd(work_dir):
+            sudo(get_extract_command(posixjoin(package_repository_path, fn)))
+
+        # Run setup.py from extracted package
+        package_name = remove_package_extension(fn)
+        extracted_package_dir = posixjoin(work_dir, package_name)
+        with cd(extracted_package_dir):
+            log_name = posixjoin(work_dir, package_name + '.log')
+            sudo('{venv_path}/bin/python setup.py install > {log_name} 2>&1'.format(**locals()))
+
+
+def install_virtualenv_contents_pip(venv_path, package_repository_path, requirements_path):
+    """Install or upgrade requirements into an existing virtualenv using PIP"""
+    sudo(('{venv_path}/bin/pip install -r {requirements_path} --no-index '
+          '--find-links {package_repository_path}').format(**locals()))
+
+
+def get_dependency_filenames(requirements_path, package_repository_path):
+    """Return a list of Python packages file names to install for the given instance"""
+    res = []
+
+    package_map = get_package_map(package_repository_path)
+    
+    for line in open(requirements_path):
+        package, version = parse_requirement(line)
+
+        if package is None or version is None:
+            continue
+        if package in env.get('skip_reqs', []):
+            continue
+
+        package_key = '%s-%s' % (package, version)
+
+        try:
+            package_file = package_map[package_key]
+            res.append(package_file)
+        except KeyError as exc:
+            raise Exception('Could not find file for dependency %s' % exc)
+
+    return res
+
+
+def filter_requirements(requirements_path, filtered_requirements_path):
+    with open(filtered_requirements_path, 'w') as filtered_file:
+        for line in open(requirements_path):
+            package, version = parse_requirement(line)
+            if package is None or version is None:
+                continue
+            if package not in env.get('skip_reqs', []):
+                filtered_file.write(line)
+
+
+def remove_comments(line):
+    """Remove comments, meaning everything after the first #"""
+    return line.split('#', 1)[0].strip()
+
+
+def parse_requirement(line):
+    """Return a tuple of a package name and version number for the given requirements line
+
+    Returns None, None if the line could not be parsed
+
+    Only supports "==" version specifications for now
+    """
+    line = remove_comments(line)
+    try:
+        package, version = line.split('==')
+    except ValueError:
+        return None, None
+    else:
+        return package.strip().lower(), version.strip().lower()
+
+
+class UnknownExtensionException(Exception):
+    pass
+
+
+PACKAGE_EXTRACT_COMMANDS = {
+    '.tar.gz': 'tar xfz "%s"',
+    '.tgz': 'tar xfz "%s"',
+    '.tar.bz2': 'tar xfj "%s"',
+    '.zip': 'unzip -q "%s"',
+}
+
+
+def remove_package_extension(fn):
+    """Remove extension from given package filename"""
+    for ext in PACKAGE_EXTRACT_COMMANDS.keys():
+        if fn.lower().endswith(ext):
+            return fn[:-len(ext)]
+
+    raise UnknownExtensionException('Package file %s has unknown extension' % fn)
+
+
+def get_extract_command(fn):
+    """Return the command needed to unpack the given file"""
+    for ext, cmd in PACKAGE_EXTRACT_COMMANDS.iteritems():
+        if fn.lower().endswith(ext):
+            return cmd % fn
+
+    raise UnknownExtensionException("Don't know how to unpack file %s" % fn)
+
+
+def get_package_map(package_repository_path):
+    """Return a dict that maps package names to package filename paths
+
+    Each key is the lowercase package name without the extension, e.g. 
+    twisted-16.3.0"""
+    repository_files = os.listdir(package_repository_path)
+
+    res = {}
+
+    for fn in repository_files:
+        try:
+            package_key = remove_package_extension(fn).lower()
+        except UnknownExtensionException:
+            pass
+        else:
+            res[package_key] = join(package_repository_path, fn)
+
+    return res
+
+
+def add_dir_to_zip(zip_file_path, path_to_add, prefix=None,
+                   ext_blacklist=None, ext_whitelist=None):
+
+    """Add all files below path_to_add to a zipfile at zip_file_path
+
+    Note that the archive is appended to, so if you want to start with an empty
+    archive, remove the file first.
+
+    If prefix is supplied, all file paths in the file are prefixed with that 
+    string.
+
+    Files having an extension in ext_blacklist are not added to the archive.
+    """
+    if ext_blacklist is None:
+        ext_blacklist = ['.pyc', '.log']
+    with closing(zipfile.ZipFile(zip_file_path, 'a', zipfile.ZIP_DEFLATED)) as zipFile:
+        for root, _dirs, files in os.walk(path_to_add):
+            for fileName in files:
+                _, ext = os.path.splitext(fileName)
+                if ext_blacklist is not None and ext in ext_blacklist:
+                    continue
+
+                if ext_whitelist is not None and ext not in ext_whitelist:
+                    continue
+
+                filePath = os.path.join(root, fileName)
+                arcname = os.path.relpath(filePath, path_to_add)
+
+                if prefix is not None:
+                    arcname = prefix + arcname
+                zipFile.write(filePath, arcname)
+
+
+@task
+def create_bliknet_user():
+    """Create user bliknet and group bliknet on remote server"""
+    group_exists = False
+    user_exists = False
+
+    group_list = run('getent group')
+    user_list = run('getent passwd')
+
+    for line in group_list.split('\n'):
+        if line.startswith('bliknet:'):
+            group_exists = True
+            break
+    for line in user_list.split('\n'):
+        if line.startswith('bliknet:'):
+            user_exists = True
+            break
+
+    if not group_exists:
+        sudo('groupadd --system bliknet')
+    else:
+        print('Not creating group bliknet: it already exists')
+    if not user_exists:
+        sudo('useradd --create-home --home-dir /opt/bliknet --gid bliknet --system bliknet')
+        # TODO, test!
+        sudo(
+            'for GROUP in adm dialout cdrom sudo audio video plugdev games users netdev input spi i2c gpio; do sudo adduser bliknet $GROUP; done')
+    else:
+        print('Not creating user bliknet: it already exists')
+
+class sudosu:
+    """Run commands as other user using "sudo su - <username>" """
+    def __init__(self, user):
+        self.user = user
+
+    def __enter__(self):
+        self.old_sudo_prefix = env.sudo_prefix
+        self.old_sudo_user, env.sudo_user = env.sudo_user, self.user
+        env.sudo_prefix = "sudo -S -p '%(sudo_prompt)s' su - %(sudo_user)s -c"
+
+    def __exit__(self, a, b, c):
+        env.sudo_prefix = self.old_sudo_prefix
+        env.sudo_user = self.old_sudo_user
---
+++ fabfile.py
@@ -0,0 +1,321 @@
+########################################
+# Fabfile to:
+#    - deploy supporting HA components
+#    - deploy HA
+########################################
+
+# Import Fabric's API module
+import sys
+from fabric.api import *
+import fabric.contrib.files
+import time
+import os
+from os.path import join
+from posixpath import join as posixjoin
+
+SCRIPT_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+# targets and common are located 1 level up (contains credentials)
+if __name__ == 'fabfile':
+    sys.path.insert(0, join(SCRIPT_DIR))
+    sys.path.insert(0, join(SCRIPT_DIR, '..'))
+
+    import targets  # noqa
+
+import common  # noqa
+
+"""env.hosts = ['localhost']
+env.user = "pi"
+env.password = "raspberry"
+env.warn_only = True
+pi_hardware = os.uname()[4] """
+
+#######################
+## Core server setup ##
+#######################
+
+def install_start():
+    """ Notify of install start """
+    print("* Warning *")
+    print("""The primary use of this installer is for a new Bliknet Node""")
+    time.sleep(10)
+    print("Installer is starting...")
+    print("Your Raspberry Pi will reboot when the installer is complete.")
+    time.sleep(5)
+
+def update_upgrade():
+    """ Update OS """
+    sudo("apt-get update")
+    sudo("apt-get upgrade -y")
+
+
+def setup_dirs():
+    """ Create all needed directories and change ownership """
+    with cd("/srv"):
+        sudo("mkdir homeassistant")
+        sudo("chown homeassistant:homeassistant homeassistant")
+        with cd("homeassistant"):
+            sudo("mkdir -p src")
+            sudo("chown homeassistant:homeassistant src")
+    with cd("/home"):
+        sudo("mkdir -p homeassistant")
+        sudo("mkdir -p /home/homeassistant/.homeassistant")
+        sudo("chown homeassistant:homeassistant homeassistant")
+    with cd("/var/lib/"):
+        sudo("mkdir mosquitto")
+        sudo("chown mosquitto:mosquitto mosquitto")
+
+
+def new_user(admin_username, admin_password):
+    env.user = 'root'
+
+    # Create the admin group and add it to the sudoers file
+    admin_group = 'admin'
+    runcmd('addgroup {group}'.format(group=admin_group))
+    runcmd('echo "%{group} ALL=(ALL) ALL" >> /etc/sudoers'.format(
+        group=admin_group))
+
+    # Create the new admin user (default group=username); add to admin group
+    runcmd('adduser {username} --disabled-password --gecos ""'.format(
+        username=pi))
+    runcmd('adduser {username} {group}'.format(
+        username=admin_username,
+        group=admin_group))
+
+    # Set the password for the new admin user
+    runcmd('echo "{username}:{password}" | chpasswd'.format(
+        username=admin_username,
+        password=admin_password))
+
+def install_syscore():
+    """ Download and install Host Dependencies. """
+    sudo("aptitude install -y build-essential")
+    sudo("aptitude install -y python-pip")
+    sudo("aptitude install -y python-dev")
+    sudo("aptitude install -y python3")
+    sudo("aptitude install -y python3-dev")
+    sudo("aptitude install -y python3-pip")
+    sudo("aptitude install -y python3-sphinx")
+    sudo("aptitude install -y python3-setuptools")
+    sudo("aptitude install -y git")
+    sudo("aptitude install -y libssl-dev")
+    sudo("aptitude install -y cmake")
+    sudo("aptitude install -y libc-ares-dev")
+    sudo("aptitude install -y uuid-dev")
+    sudo("aptitude install -y daemon")
+    sudo("aptitude install -y curl")
+    sudo("aptitude install -y libgnutls28-dev")
+    sudo("aptitude install -y libgnutlsxx28")
+    sudo("aptitude install -y libgnutls-dev")
+    sudo("aptitude install -y nmap")
+    sudo("aptitude install -y net-tools")
+    sudo("aptitude install -y sudo")
+    sudo("aptitude install -y libglib2.0-dev")
+    sudo("aptitude install -y cython3")
+    sudo("aptitude install -y libudev-dev")
+    sudo("aptitude install -y libxrandr-dev")
+    sudo("aptitude install -y swig")
+
+def install_pycore():
+    """ Download and install VirtualEnv """
+    sudo("pip3 install --upgrade pip")
+    sudo("pip3 install virtualenv")
+
+def create_venv():
+    """ Create home-assistant VirtualEnv """
+    with cd("/srv/homeassistant"):
+            sudo("virtualenv -p python3 homeassistant_venv", user="homeassistant")
+
+
+#######################################################
+## Build and Install Applications without VirtualEnv ##
+#######################################################
+
+def setup_homeassistant_novenv():
+    """ Install Home-Assistant """
+    sudo("pip3 install --upgrade pip", user="homeassistant")
+    sudo("pip3 install homeassistant", user="homeassistant")
+
+def setup_openzwave_novenv():
+    """ Install python-openzwave """
+    sudo("pip3 install --upgrade cython==0.24.1", user="homeassistant")
+    with cd("/srv/homeassistant/src"):
+        sudo("git clone https://github.com/OpenZWave/python-openzwave.git", user="homeassistant")
+        with cd("python-openzwave"):
+            sudo("git checkout python3", user="homeassistant")
+            sudo("make build", user="homeassistant")
+            sudo("make install", user="homeassistant")
+
+def setup_services_novenv():
+    """ Enable applications to start at boot via systemd """
+    hacfg="""
+mqtt:
+  broker: 127.0.0.1
+  port: 1883
+  client_id: home-assistant-1
+  username: pi
+  password: raspberry
+"""
+    with cd("/etc/systemd/system/"):
+        put("home-assistant_novenv.service", "home-assistant_novenv.service", use_sudo=True)
+    with settings(sudo_user='homeassistant'):
+        sudo("/srv/homeassistant/homeassistant_venv/bin/hass --script ensure_config --config /home/homeassistant/.homeassistant")
+
+    fabric.contrib.files.append("/home/homeassistant/.homeassistant/configuration.yaml", hacfg, use_sudo=True)
+    sudo("systemctl enable home-assistant_novenv.service")
+    sudo("systemctl daemon-reload")
+
+def setup_libcec_novenv():
+    """ Install libcec according to https://github.com/Pulse-Eight/libcec/blob/master/docs/README.raspberrypi.md """
+    with cd("/srv/homeassistant/src"):
+        sudo("git clone https://github.com/Pulse-Eight/platform.git", user="homeassistant")
+        sudo("mkdir platform/build", user="homeassistant")
+        with cd("platform/build"):
+            sudo("cmake ..", user="homeassistant")
+            sudo("make", user="homeassistant")
+            sudo("make install")
+        sudo("git clone https://github.com/Pulse-Eight/libcec.git", user="homeassistant")
+        sudo("mkdir libcec/build", user="homeassistant")
+        with cd("libcec/build"):
+            sudo("cmake -DRPI_INCLUDE_DIR=/opt/vc/include -DRPI_LIB_DIR=/opt/vc/lib ..", user="homeassistant")
+            sudo("make -j4", user="homeassistant")
+            sudo("make install")
+            sudo("ldconfig")
+
+####################################
+## Build and Install Applications ##
+####################################
+
+def setup_mosquitto():
+    """ Install Mosquitto w/ websockets"""
+    with cd("/srv/homeassistant/src"):
+        sudo("curl -O http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key")
+        sudo("apt-key add mosquitto-repo.gpg.key")
+        with cd("/etc/apt/sources.list.d/"):
+            sudo("curl -O http://repo.mosquitto.org/debian/mosquitto-jessie.list")
+            sudo("apt-get update")
+            sudo("apt-cache search mosquitto")
+            sudo("apt-get install -y mosquitto mosquitto-clients")
+            with cd("/etc/mosquitto"):
+                put("mosquitto.conf", "mosquitto.conf", use_sudo=True)
+                sudo("touch pwfile")
+                sudo("chown mosquitto: pwfile")
+                sudo("chmod 0600 pwfile")
+                sudo("sudo mosquitto_passwd -b pwfile pi raspberry")
+                sudo("sudo chown mosquitto: mosquitto.conf")
+
+def setup_homeassistant():
+    """ Activate Virtualenv, Install Home-Assistant """
+    sudo("source /srv/homeassistant/homeassistant_venv/bin/activate && pip3 install homeassistant", user="homeassistant")
+    with cd("/home/homeassistant/"):
+        sudo("chown -R homeassistant:homeassistant /home/homeassistant/")
+
+def setup_openzwave():
+    """ Activate Virtualenv, Install python-openzwave"""
+    sudo("source /srv/homeassistant/homeassistant_venv/bin/activate && pip3 install --upgrade cython==0.24.1", user="homeassistant")
+    with cd("/srv/homeassistant/src"):
+        sudo("git clone --branch v0.3.3 https://github.com/OpenZWave/python-openzwave.git", user="homeassistant")
+        with cd("python-openzwave"):
+            sudo("git checkout python3", user="homeassistant")
+            sudo("source /srv/homeassistant/homeassistant_venv/bin/activate && make build", user="homeassistant")
+            sudo("source /srv/homeassistant/homeassistant_venv/bin/activate && make install", user="homeassistant")
+
+def setup_libcec():
+    setup_libcec_novenv()
+    sudo("ln -s /usr/local/lib/python3.4/dist-packages/cec /srv/homeassistant/homeassistant_venv/lib/python3.4/site-packages", user="homeassistant")
+
+def setup_libmicrohttpd():
+    """ Build and install libmicrohttpd """
+    with cd("/srv/homeassistant/src"):
+        sudo("mkdir libmicrohttpd")
+        sudo("chown homeassistant:homeassistant libmicrohttpd")
+        sudo("curl -O ftp://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.19.tar.gz", user="homeassistant")
+        sudo("tar zxvf libmicrohttpd-0.9.19.tar.gz")
+        with cd("libmicrohttpd-0.9.19"):
+            sudo("./configure")
+            sudo("make")
+            sudo("make install")
+
+def setup_openzwave_controlpanel():
+    """ Build and Install open-zwave-control-panel """
+    with cd("/srv/homeassistant/src"):
+        sudo("git clone https://github.com/OpenZWave/open-zwave-control-panel.git", user="homeassistant")
+        with cd("open-zwave-control-panel"):
+            put("Makefile", "Makefile", use_sudo=True)
+            sudo("make")
+            if pi_hardware == "armv7l":
+                sudo("ln -sd /srv/homeassistant/homeassistant_venv/lib/python3.4/site-packages/libopenzwave-0.3.3-py3.4-linux-armv7l.egg/config")
+            else:
+                sudo("ln -sd /srv/homeassistant/homeassistant_venv/lib/python3.4/site-packages/libopenzwave-0.3.3-py3.4-linux-armv**6**l.egg/config")
+        sudo("chown -R homeassistant:homeassistant /srv/homeassistant/src/open-zwave-control-panel")
+
+def setup_services():
+    """ Enable applications to start at boot via systemd """
+    with cd("/etc/systemd/system/"):
+        put("home-assistant.service", "home-assistant.service", use_sudo=True)
+    with settings(sudo_user='homeassistant'):
+        sudo("/srv/homeassistant/homeassistant_venv/bin/hass --script ensure_config --config /home/homeassistant/.homeassistant")
+
+
+    hacfg="""
+mqtt:
+  broker: 127.0.0.1
+  port: 1883
+  client_id: home-assistant-1
+  username: pi
+  password: raspberry
+"""
+
+
+    fabric.contrib.files.append("/home/homeassistant/.homeassistant/configuration.yaml", hacfg, use_sudo=True)
+    sudo("systemctl enable home-assistant.service")
+    sudo("systemctl daemon-reload")
+    sudo("systemctl start home-assistant.service")
+def upgrade_homeassistant():
+    """ Activate Venv, and upgrade Home Assistant to latest version """
+    sudo("source /srv/homeassistant/homeassistant_venv/bin/activate && pip3 install homeassistant --upgrade", user="homeassistant")
+
+#############
+## Deploy! ##
+#############
+
+def deploy():
+    ## Install Start ##
+    install_start()
+    ## Initial Update and Upgrade ##
+    update_upgrade()
+    ## Setup service accounts ##
+    common.create_bliknet_user()
+
+    ## Setup directories ##
+    setup_dirs()
+
+    ## Install dependencies ##
+    install_syscore()
+    install_pycore()
+
+    ## Create VirtualEnv ##
+    create_venv()
+
+    ## Build and Install Mosquitto ##
+    setup_mosquitto()
+
+    ## Activate venv, install Home-Assistant ##
+    setup_homeassistant()
+
+    ## Make apps start at boot ##
+    setup_services()
+
+    ## Activate venv, build and install python-openzwave ##
+    setup_openzwave()
+
+    ## Build and install libmicrohttpd ##
+    setup_libmicrohttpd()
+
+    ## Build and install open-zwave-control-panel ##
+    setup_openzwave_controlpanel()
+
+    ## Build and install libcec ##
+    setup_libcec()
+
+    ## Reboot the system ##
+    reboot()
